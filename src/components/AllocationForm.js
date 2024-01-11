bb3a2e02e0fd2f0a6796100f28731fdb0ffce2e7
Andre Clarke <fnaw2000@yahoo.com>
2024-01-11 17:24:59 -0500

    changes made from theia lab environment

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,8 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  
+                  <span style={{ "position":"relative","right":"-25px"}}> $ </span>   
                     <input
                         required='required'
                         type='number'
@@ -67,6 +68,8 @@ const AllocationForm = (props) => {
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
+                    
+                
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FcPlus} from "react-icons/fc";
+import { AiFillMinusCircle} from "react-icons/ai";
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -17,9 +19,20 @@ const ExpenseItem = (props) => {
             name: name,
             cost: 10,
         };
-
         dispatch({
             type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
             payload: expense
         });
 
@@ -28,8 +41,9 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>${props.cost}</td>
+        <td><FcPlus size='2.2em' onClick={event=> increaseAllocation(props.name)}></FcPlus></td>
+        <td><AiFillMinusCircle size='2.2em' color="red" onClick={event=> decreaseAllocation(props.name)}></AiFillMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
